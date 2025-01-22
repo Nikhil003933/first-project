4616f74ec3a093c60ee3b8e347880cd45435e98a
Nikhil <nikhilkumar20160011@gmail.com>
2025-01-22 22:44:11 +0530

    first commit

---
+++ exam.py
@@ -0,0 +1,15 @@
+print("hello dude")
+print('i am AI')
+def can_drive(age):
+    if age==18:
+        print("you are eligible for drive")
+    elif age >19:
+        print("may be you are good in driving")
+    else:
+         print("you cannot drive")
+
+age=int(input("enter the age of candidate="))
+
+can_drive(age)
+
+print(can_drive(age).__doc__)
